b875abf0e30bf17b0c950027e736173788f0e6c3
hema <hema@helijia.com>
2016-12-30 17:51:40 +0800

    add feature vux

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,43 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <router-link to="/hello">hello</router-link>
-    <router-link to="/welcome">welcome</router-link>
+    <divider>Horizontal</divider>
+    <flexbox style="height:40px;">
+      <flexbox-item><div class="flex-demo">1</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">2</div></flexbox-item>
+    </flexbox>
+    <divider>Vertical</divider>
+    <flexbox orient="vertical" :margin-left=0>
+      <flexbox-item><div class="flex-demo" style="margin-left:0">1</div></flexbox-item>
+      <flexbox-item><div class="flex-demo" style="margin-left:0">2</div></flexbox-item>
+    </flexbox>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Group from 'vux/src/components/group'
+import Cell from 'vux/src/components/cell'
+import Divider from 'vux/src/components/divider'
+import {Flexbox, FlexboxItem} from 'vux/src/components/flexbox'
 
 export default {
-  name: 'app',
+  data () {
+     return {
+
+     }
+   },
   components: {
-    Hello
+    Group,
+    Cell,
+    Flexbox,
+    FlexboxItem,
+    Divider
   }
 }
 </script>
 
 <style>
+@import '~vux/dist/vux.css';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,5 +45,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.flex-demo {
+  text-align: center;
+  color: #fff;
+  background-color: #20b907;
+  margin-bottom: 8px;
+  border-radius: 4px;
+  -webkit-background-clip: padding-box;
 }
 </style>
